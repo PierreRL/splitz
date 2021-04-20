e5279f625c863a680aba49b5a25f0fc99fb4c447
Pierre Lardet <lardet.pierre@gmail.com>
2021-04-20 23:03:18 +0100

    still brokey

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     Distance {{ distance }} <input type="number" v-model="distance" /> <br />
-    Time {{ time.minutes }} : {{ time.seconds }}
-    <input type="number" v-model="time.minutes" />:<input type="number" v-model="time.seconds" /> <br />
-    Pace {{ pace.minutes }} : {{ pace.seconds }} min/km
+    Time {{ dtime.minutes }} : {{ dtime.seconds }}
+    <input
+      type="number"
+      v-model="dtime.minutes"
+    />
+    :
+    <input
+      type="number"
+      v-model="dtime.seconds"
+      min="0"
+      max="59"
+    />
+    <br />
+    Pace {{ dpace.minutes }} : {{ dpace.seconds }} min/km
   </div>
 </template>
 
@@ -11,85 +22,65 @@
 import { Vue } from "vue-class-component";
 
 export default class Home extends Vue {
-  /* private distance = 0;
-  public time = new Time(0);
-  private _pace:Time = new Time(0); 
+  distance = 0;
 
+  get dtime(): StringTime {
+    return {
+      minutes: this.time.minutes.toString(),
+      seconds: this.time.seconds.toString(),
+    };
+  }
+  set dtime(stringTime : StringTime) {
+    console.log(stringTime);
+
+    this.time.minutes = parseInt(stringTime.minutes);
+    this.time.seconds = parseInt(stringTime.seconds);
+  }
+  get dpace(): StringTime {
+    return {
+      minutes: this.pace.minutes.toString(),
+      seconds: this.pace.seconds.toString(),
+    };
+  }
+  p_time: Time = { minutes: 0, seconds: 0 };
+  get time(): Time {
+    return this.p_time;
+  }
+  set time(time: Time) {
+    if (time.seconds >= 60 || time.seconds < 0 || time.minutes < 0) {
+      return;
+    } else {
+      this.p_time = time;
+    }
+  }
   get pace(): Time {
     if (this.distance == 0) {
-      return new Time(0);
+      return { minutes: 0, seconds: 0 };
     }
-    return new Time((this.time.timeInSecs / this.distance));
-  } */
-  public distance = 0;
-  public time: Time = {minutes:0, seconds:0};
-  public pace = function() : Time {
-    /* const  = this.
-    return {minutes: } */
+    const totalSecondsTime = this.time.minutes * 60 + Number(this.time.seconds);
+    const totalDistance = this.distance;
+    const totalPace = totalSecondsTime / totalDistance;
+    const paceMinutes = Math.floor(totalPace / 60);
+    const paceSeconds = totalPace - 60 * paceMinutes;
+    const roundedMins = Math.round(paceMinutes);
+    const roundedSeconds = Math.round(paceSeconds);
+    return { minutes: roundedMins, seconds: roundedSeconds };
   }
 }
-interface Time {
-  minutes: number,
-  seconds: number
-}
-/* class Time {
-  private _minutes = 0;
 
-  get minutes(): number {
-    if (!this._seconds) {
-      return 0;
-    }
-    return this._minutes;
+/* function padNumber2(number: number): string {
+  let output : string = number + "";
+  while (output.length < 2) {
+    output = "0" + output;
   }
-
-  set minutes(minutes: number) {
-    if (minutes < 0) {
-      return;
-    } else {
-      this._minutes = minutes;
-    }
-  }
-
-  private _seconds = 0;
-  get seconds(): number {
-    if (!this._seconds) {
-      return 0;
-    }
-    return this._seconds;
-  }
-  set seconds(seconds: number) {
-    if (seconds < 0) {
-      return;
-    } else {
-      this._seconds = seconds;
-      this.computeTotalTime();
-    }
-  }
-  private _timeInSecs = 0;
-  get timeInSecs(): number {
-    return this._timeInSecs;
-  }
-  set timeInSecs(timeInSecs: number) {
-    if (timeInSecs < 0) {
-      return;
-    } else {
-      this._timeInSecs = timeInSecs;
-      this.computeTotalTime();
-    }
-  }
-
-  private computeTotalTime() {
-    this._timeInSecs = this._seconds + (this._minutes * 60);
-  }
-  constructor(timeInSecs: number) {
-    this.setAllValues(timeInSecs);
-  }
-
-  setAllValues(timeInSecs: number) {
-    this._timeInSecs = timeInSecs;
-    this._minutes = Math.floor(timeInSecs / 60);
-    this._seconds = timeInSecs - this.minutes * 60;
-    this._milliseconds = Math.floor(timeInSecs / 1000) - this.seconds;
-  }
+  return output;
 } */
+interface Time {
+  minutes: number;
+  seconds: number;
+}
+interface StringTime {
+  minutes: string;
+  seconds: string;
+}
 </script>
