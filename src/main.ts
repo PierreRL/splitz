175c0cb4310f260a54f7bfa8fad8a9209af9afc1
Pierre Lardet <lardet.pierre@gmail.com>
2021-06-27 20:00:06 +0100

    google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag-next";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(VueGtag, {
+    property: {
+        id: "277299114"
+    }
+}, router);
+
+app.mount("#app");
